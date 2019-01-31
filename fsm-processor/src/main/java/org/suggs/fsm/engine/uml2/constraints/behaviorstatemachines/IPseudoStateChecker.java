ab301ca5959341a896874da945002f0576408877
Peter Suggitt <me@suggs.org.uk>
2019-01-31 19:08:53 +0000

    removed legacy

--- fsm-processor/src/main/java/org/suggs/fsm/engine/uml2/constraints/behaviorstatemachines/IPseudoStateChecker.java
+++
@@ -1,8 +0,0 @@
-package org.suggs.fsm.engine.uml2.constraints.behaviorstatemachines;
-
-import org.suggs.fsm.uml2.behaviorstatemachines.IPseudoState;
-
-public interface IPseudoStateChecker {
-
-    void checkConstraints(IPseudoState pseudostate);
-}
--- fsm-processor/src/main/java/org/suggs/fsm/uml2/kernel/IClassifier.java
+++
@@ -1,4 +0,0 @@
-package org.suggs.fsm.uml2.kernel;
-
-public interface IClassifier extends INamespace, INamedElement {
-}
