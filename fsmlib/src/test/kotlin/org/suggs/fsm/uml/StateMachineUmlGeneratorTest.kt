400c64f01177e20a66b9d2c56e5d36207d677a67
Peter Suggitt <me@suggs.org.uk>
2021-10-05 08:01:24 +0100

    updated puml tests to read better

--- fsmlib/src/test/kotlin/org/suggs/fsm/uml/StateMachineUmlGeneratorTest.kt
+++ fsmlib/src/test/kotlin/org/suggs/fsm/uml/StateMachineUmlGeneratorTest.kt
@@ -14,36 +14,60 @@ import org.suggs.fsm.uml.StateMachineUmlGenerator.Companion.writePumlToFile
 
 class StateMachineUmlGeneratorTest {
 
-    @Test fun `describes simple state machines`() {
-        writePumlToFile(generateUmlFor(simpleFsmPrototype().build()), "simpleFsm.puml")
+    @Test
+    fun `describes simple state machines`() {
+        simpleFsmPrototype().build()
+            .let { generateUmlFor(it) }
+            .let { writePumlToFile(it, "simpleFsm.puml") }
     }
 
-    @Test fun `describes simple state machines with two routes`() {
-        writePumlToFile(generateUmlFor(fsmWithTwoOutcomesPrototype().build()), "simpleFsmLeftAndRight.puml")
+    @Test
+    fun `describes simple state machines with two routes`() {
+        fsmWithTwoOutcomesPrototype().build()
+            .let { generateUmlFor(it) }
+            .let { writePumlToFile(it, "simpleFsmLeftAndRight.puml") }
     }
 
-    @Test fun `describes state machines with deferred events`() {
-        writePumlToFile(generateUmlFor(fsmWithDeferredTransitionsPrototype().build()), "deferredTransitions.puml")
+    @Test
+    fun `describes state machines with deferred events`() {
+        fsmWithDeferredTransitionsPrototype().build()
+            .let { generateUmlFor(it) }
+            .let { writePumlToFile(it, "deferredTransitions.puml") }
     }
 
-    @Test fun `describes state machine with deferred and automated transitions`() {
-        writePumlToFile(generateUmlFor(fsmWithDeferredAndAutomatedTransitionsPrototype().build()), "deferredAndAutomatedTransitions.puml")
+    @Test
+    fun `describes state machine with deferred and automated transitions`() {
+        fsmWithDeferredAndAutomatedTransitionsPrototype().build()
+            .let { generateUmlFor(it) }
+            .let { writePumlToFile(it, "deferredAndAutomatedTransitions.puml") }
     }
 
-    @Test fun `describes a simple composite State machine`() {
-        writePumlToFile(generateUmlFor(simpleNestedStatemachineProtoType { true }.build()), "simpleCompositeFsm.puml")
+    @Test
+    fun `describes a simple composite State machine`() {
+        simpleNestedStatemachineProtoType { true }.build()
+            .let { generateUmlFor(it) }
+            .let { writePumlToFile(it, "simpleCompositeFsm.puml") }
     }
 
-    @Test fun `describes composite state machines`() {
-        writePumlToFile(generateUmlFor(nestedStateStateMachinePrototype().build()), "compositeFsm.puml")
+    @Test
+    fun `describes composite state machines`() {
+        nestedStateStateMachinePrototype().build()
+            .let { generateUmlFor(it) }
+            .let { writePumlToFile(it, "compositeFsm.puml") }
     }
 
-    @Test fun `describes a composite state machine with guard condition to close`() {
-        writePumlToFile(generateUmlFor(nestedStateStateMachineWithGuardExitPrototype { true }.build()), "compositeWithGuardExitFsm.puml")
+    @Test
+    fun `describes a composite state machine with guard condition to close`() {
+        nestedStateStateMachineWithGuardExitPrototype { true }.build()
+            .let { generateUmlFor(it) }
+            .let { writePumlToFile(it, "compositeWithGuardExitFsm.puml") }
     }
 
-    @Test fun `describes state machines that have states with entry and exit behaviors`() {
-        writePumlToFile(generateUmlFor(fsmWithEntryAndExitBehaviorsPrototype().build()), "entryAndExitBehaviors.puml")
+    @Test
+    fun `describes state machines that have states with entry and exit behaviors`() {
+        fsmWithEntryAndExitBehaviorsPrototype().build()
+            .let { generateUmlFor(it) }
+            .let { writePumlToFile(it, "entryAndExitBehaviors.puml") }
     }
 
 }
